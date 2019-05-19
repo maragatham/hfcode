ab0dc442312a47b7bbc6e6aa0d6870effe5efa64
Maragatham <umaa@live.in>
2019-05-19 23:37:56 +0530

    Dog Calculator code

---
+++ chapter_2/dogcalc.py
@@ -0,0 +1,9 @@
+dog_name = input("What is your dog's name ")
+
+dog_age = input("What is your dog's age ")
+
+Dogage_human = int(dog_age) * 7
+
+print(dog_name + " is " + str(Dogage_human) + " old")
+
+
